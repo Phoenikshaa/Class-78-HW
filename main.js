b22e76f19c2782db209028c9fca98d9e24e145f3
Phoenikshaa <77309769+Phoenikshaa@users.noreply.github.com>
2021-05-14 17:42:01 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var images = ["https://images.app.goo.gl/93tDoocG2yW3z48WA", "https://images.app.goo.gl/7jYYv9DVEmjy3F5D9", "https://images.app.goo.gl/gzFrRPhcwK8cm4Wh7", "https://images.app.goo.gl/yBJoGqyDKFpWGRJa6", "https://images.app.goo.gl/2srK6WhoKc98qZVk8"];
+var images = ["Family Book . png ", "Kylie Cantrall .png", "Ariana Grande . png", "Billie Eilish .png", "Pilar Pascual . png"];
 var names = ["Family Book", "Kylie Cantrall", "Ariana Grande", "Billie Eilish", "Pilar Pascual"];
 var i = 0;
 function update() {
